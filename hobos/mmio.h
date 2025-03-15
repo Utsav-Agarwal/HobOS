bfdc18c828395b6af557c19c25741f3e7b56c06a
Utsav Agarwal <utsavagarwal2014@gmail.com>
2025-03-15 22:15:06 +0000

    fixing mmio header after merge
    Signed-off-by: Utsav Agarwal <utsavagarwal2014@gmail.com>

--- hobos/mmio.h
+++ hobos/mmio.h
@@ -1,5 +1,5 @@
-#ifndef __MMIO_H
-#define __MMIO_H
+#ifndef __MMIO_H_
+#define __MMIO_H_
 
 #include <stdint.h>
 
@@ -19,49 +19,10 @@
 #define CLEAR_REG(reg_addr, reg_size) \
 	*(u##reg_size *) reg_addr = 0
 
-//TODO: implement feature for runtime detection
-extern uint8_t rpi_version;
-
-inline void get_rpi_version(void)
-{
-	rpi_version = 3;
-}
-
-inline void mmio_write(uint32_t offset, uint32_t val)
-{
-	*(volatile uint32_t *)(mmio_base + offset) = val;
-}
-
-inline uint32_t mmio_read(uint32_t offset)
-{
-	return *(volatile uint32_t *)(mmio_base + offset);
-}
-
-//TODO: add rpi 5
-inline void mmio_init(uint8_t rpi_version)
-{
-	switch(rpi_version)
-	{
-		case 3:
-			mmio_base = 0x3f000000;
-			break;
-		case 4:
-			mmio_base = 0xfe000000;
-			break;
-		case 5:
-			//uart offset: 0x30000
-			//uart address: 0x107d001000
-			//uart address = BAR + offset
-			mmio_base = 0x107cfd1000;
-			break;
-		default:
-			mmio_base = 0x20000000;
-	}
-}
-
-inline void delay(int32_t count)
-{
-	while(count--) {asm volatile("nop");}
-}
+void get_rpi_version(void);
+void mmio_write(uint32_t offset, uint32_t val);
+uint32_t mmio_read(uint32_t offset);
+void mmio_init(void);
+void delay(uint32_t count);
 
 #endif
