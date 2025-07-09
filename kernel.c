e0df708a6528db25a67b36894fe5726807d8c3f9
Utsav Agarwal <utsavagarwal2014@gmail.com>
2025-07-09 19:08:44 +0100

    Adding support for secondary cores to jump onto other functions
    Signed-off-by: Utsav Agarwal <utsavagarwal2014@gmail.com>

--- kernel.c
+++ kernel.c
@@ -6,12 +6,34 @@
 
 extern int setup_stack(void);
 
+
+void test_print1(void)
+{
+	kprintf("Hello again 1\n");
+	return;
+}
+
+void test_print2(void)
+{
+	kprintf("Hello again 2\n");
+	return;
+}
+
 /* I'm alive */
 void heartbeat(void)
 {
 	run_process((uint64_t) setup_stack, 1);
 	run_process((uint64_t) setup_stack, 2);
-	run_process((uint64_t) setup_stack, 3);
+
+	//TODO: threading implementation
+	//with proper mutex implementation, we
+	//should be able to see both these functions executed
+	run_process((uint64_t) test_print1, 1);
+	run_process((uint64_t) test_print1, 2);
+	run_process((uint64_t) test_print1, 2);
+	run_process((uint64_t) test_print1, 2);
+	run_process((uint64_t) test_print2, 1);
+
 }
 
 void main()
--- smp.c
+++ smp.c
@@ -5,25 +5,46 @@ extern int curr_core_id(void);
 
 uint64_t *cpu_spin_table = (uint64_t *) SPIN_TABLE_BASE;
 
+//TODO: We need a mutex here to preserve a read/write order
+//for the spin table
+
 int run_process(uint64_t fn_addr, uint8_t cpu_id)
 {
 
-    if (cpu_id > MAX_REMOTE_CORE_ID)
-	return -1;
+	if (cpu_id > MAX_REMOTE_CORE_ID)
+		return -1;
+	
+	cpu_spin_table[cpu_id] = fn_addr;
+	
+	//data sync barrier
+	__asm__ volatile("dsb sy");
+	__asm__ volatile("sev");
 
-    cpu_spin_table[cpu_id] = fn_addr;
-
-    //wake up processors with "SEND EVENT"
-    __asm__ volatile("sev");
-
-    return 0;
+	return 0;
 }
 
+
+//TODO:
+//if we attempt to clear or set the spin table, we might
+//lose a read or write incoming from another processor.
+//
+//This is why we need a mutex before queueing more than
+//one processors
+//
+//This current implementation works, just about, with the 
+//current implementation, but has no guarantees.
 void park_and_wait (void)
 {
-	kprintf ("Hello from core %d\n", curr_core_id());
+	uint8_t core_id = curr_core_id();
 
 	while (1) {
+	
+		//data sync barrier
+		__asm__ volatile("dsb sy");
+		
+		if (cpu_spin_table[core_id]) 
+			( (void (*)(void)) cpu_spin_table[core_id])();
+
 		__asm__ volatile ("wfe");
 	}
 }
