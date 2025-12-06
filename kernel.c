6170a3ff09e29cb204dc718b62d70d3bcc881d8b
Utsav Agarwal <utsavagarwal2014@gmail.com>
2025-12-06 23:59:18 +0530

    smp: Adding barriers and coherence related functionality
    Signed-off-by: Utsav Agarwal <utsavagarwal2014@gmail.com>

--- kernel.c
+++ kernel.c
@@ -7,26 +7,47 @@
 #include "hobos/gpio.h"
 #include "hobos/mmu.h"
 
-extern int setup_stack(void);
 extern uint8_t curr_core_el(void);
+extern uint8_t curr_core_id(void);
 extern void switch_el(void);
 
 #define KERNEL_START	0xFFFFFF8000000000
 #define USER_END        ((volatile unsigned int*) (KERNEL_START + 0x1f000))
-
-/* I'm alive */
-void heartbeat(void)
-{
-	*USER_END = 0xcacacaca;	
-}
 
 void setup_console() 
 {
 	struct gpio_controller ctrl;
 
 	init_console(&ctrl);
-	kprintf("\n\nConsole set\n");
+	kprintf("\n\nConsole set by %d\n", curr_core_id());
 }
+
+
+int cntr = 0;
+void test(void) 
+{
+	//setup_console();
+	smp_store_release(&cntr, READ_ONCE(cntr)+1); 	
+	kprintf("Hello again %d\n", cntr);
+}
+smp_process(test, 1)
+smp_process(test, 2)
+
+extern void setup_stack(void);
+
+/* I'm alive */
+void heartbeat(void)
+{
+	__run_process((uint64_t) setup_stack, 1);
+	smp_store_release(&semaphores[1], 0); 
+	
+	__run_process((uint64_t) setup_stack, 2);
+	smp_store_release(&semaphores[2], 0); 
+
+	smp_run_process(test, 1);
+	smp_run_process(test, 2);
+}
+
 
 void kernel_panic()
 {
@@ -44,11 +65,8 @@ void init_kernel()
 void main()
 {
 
-	init_kernel();
+	//init_kernel();
 	heartbeat();
-
-	//should trigger data abort
-	//setup_console();
 
 	while (1) {
 		//start shell here
