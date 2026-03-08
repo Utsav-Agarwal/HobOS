Utsav-Agarwal/HobOS

commit 60786007c8e26cde95a1e832ef1913a62b31d973

pt: Consolidate page table related operations
move fundamental page table ops and definitions such as identity page table
mapping and global page size under the pt_lib library

Signed-off-by: Utsav Agarwal <utsavagarwal2014@gmail.com>

page_alloc.c
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: GPL-2.0-only
 
 #include <hobos/page_alloc.h>
+#include <hobos/mmu.h>
 #include <hobos/compiler_types.h>
 #include <hobos/kstdio.h>
 
@@ -298,6 +299,7 @@ static int split_page_block(int order)
 {
 	volatile struct page_block *pb = get_next_free_block(order);
 	u64 base_addr = 0;
+
 	__unused u64 page_offset = 0;
 	int curr_order = order;
 
@@ -431,9 +433,8 @@ static int get_page_order(size_t nr_pages)
 	size_t val = nr_pages;
 	int i = 0;
 
-	while (val != PAGE_BLOCK(i)) {
+	while (val < PAGE_BLOCK(i))
 		i++;
-	}
 
 	return i;
 }

include/hobos/lib/pt_lib.h
@@ -23,7 +23,7 @@
 //AP Flag (Access permissions)
 
 #define PT_KERNEL	(0b00 << PT_AP_POS)
-#define PT_USER	(0b01 << PT_AP_POS)
+#define PT_USER		(0b01 << PT_AP_POS)
 #define PT_AP_RW	(0b00 << PT_AP_POS)
 #define PT_AP_RO	(0b10 << PT_AP_POS)
 
@@ -97,13 +97,19 @@ struct va_metadata {
 	unsigned long offset;
 };
 
-//extract pte desc/meta data - more for debugging purposes
-//struct *unsigned long_desc get_pte(void *);
-
 unsigned long pt_entry(unsigned long paddr, unsigned long flags);
-void place_pt_entry(struct page_table_desc *pt_desc, unsigned long pte, int index);
+void place_pt_entry(struct page_table_desc *pt_desc, unsigned long pte,
+		    int index);
+
 struct page_table_desc *create_pt(unsigned long pt_baddr, char level);
-volatile u64 *create_pt_entries(
-		struct page_table_desc *pt_desc,
-		unsigned long start_paddr, unsigned long end_paddr, unsigned long flags);
+volatile u64 *create_pt_entries(struct page_table_desc *pt_desc,
+				unsigned long start_paddr,
+				unsigned long end_paddr,
+				unsigned long flags);
+
+void map_pa_to_va_pg(unsigned long pa, unsigned long va,
+		     struct page_table_desc *pt_top,
+		     unsigned long flags);
+
+void create_id_mapping(u64 start_paddr, u64 end_paddr, u64 pt, u64 flags);
 #endif

include/hobos/mmu.h
@@ -117,7 +117,8 @@ struct tcr_el1_cfg {
 #define MAIR_MEM_I_WB_NT(r, w)	MAIR_MEM_RW(0b11, r, w)
 
 //kernel offset after switch to high memory addressing
-#define KERNEL_START	0xFFFFFF8000000000
+#define KERNEL_START	0xFFFFFF8000000000UL
+#define PAGE_SIZE	KB(4)
 
 struct mair_attr {
 	char outer_cache: 4;
@@ -138,10 +139,6 @@ extern u8 pt_ctr;
 
 void init_mmu(void);
 void switch_vmem(void);
-void map_pa_to_va_pg(unsigned long pa, unsigned long va,
-		     struct page_table_desc *pt_top,
-		     unsigned long flags);
-void create_id_mapping(u64 start_paddr, u64 end_paddr,
-			u64 pt, u64 flags);
+void set_ttbr0_el1(u64 x);
 
 #endif

include/hobos/mmu/bcm2835.h
@@ -6,7 +6,6 @@
 #include "../mmu.h"
 
 //TODO: use linker script definitions
-#define PAGE_SIZE	KB(4)
 #define TABLE_BADDR	(unsigned long)(&__end)
 #define TTBR1_OFFSET_B	0
 #define TTBR1_OFFSET	0

include/hobos/page_alloc.h
@@ -11,8 +11,6 @@
 #define PAGE_BLOCK(order)	(1 << (order))
 #define MAX_PAGE_BLOCK	PAGE_BLOCK(MAX_PAGE_ORDER)
 
-#define PAGE_SIZE	4096UL
-
 #define BLOCK_USED	0xDEDE
 
 struct page_block {

mmu.c
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: GPL-2.0-only
 
 #include <hobos/asm/barrier.h>
-#include <hobos/kstdio.h>
+#include <hobos/compiler_types.h>
 #include <hobos/mmu/bcm2835.h>
 #include <hobos/mmu.h>
 #include <hobos/smp.h>
@@ -9,95 +9,9 @@
 #define map_sz		512
 #define ID_PG_SZ	PAGE_SIZE
 
-void create_id_mapping(u64 start_paddr, u64 end_paddr,
-			u64 pt, u64 flags)
-{
-	//for now lets assume T0/1_SZ is constant at 25, so we
-	//only care about 3 levels
-	u64 end_addr;
-	struct page_table_desc *pt_desc;
-
-	pt_desc = create_pt(pt, 1);
-	end_addr = (u64)(pt_desc->pt) + NEXT_PT_OFFSET;
-	create_pt_entries(pt_desc, end_addr, end_addr, flags);
-
-	pt_desc = create_pt(end_addr, 2);
-	end_addr += NEXT_PT_OFFSET;
-	create_pt_entries(pt_desc, end_addr, end_addr, flags);
-
-	pt_desc = create_pt(end_addr, 3);
-	create_pt_entries(pt_desc, start_paddr, end_paddr, flags); //2GB
-}
-
-static inline void extract_va_metadata(u64 va, struct va_metadata *meta)
-{
-	u64 mask = 0xFFF;
-
-	meta->offset = va & mask;	//offset is calculated with last
-					//12 bits
-
-	mask = BITM(9);		//9 bits bitmask
-
-	meta->index[0] = (va >> 30) & mask;
-	meta->index[1] = (va >> 21) & mask;
-	meta->index[2] = (va >> 12) & mask;
-}
-
-static inline u64 pte_is_empty(u64 pte)
-{
-	return !(pte);
-}
-
-void map_pa_to_va_pg(u64 pa, u64 va, struct page_table_desc *pt_top,
-		     u64 flags)
-{
-	struct page_table_desc *pt_desc = pt_top;
-	u64 volatile *pt, pte;
-	struct va_metadata meta;
-	u16 pt_index;
-	u64 pte_flags = flags;
-	u8 level, i;
-
-	extract_va_metadata(va, &meta);
-	meta.offset += pa;	//we want physical address at the end
-
-	for (i = pt_desc->level; i <= PT_LVL_MAX; i++) {
-		level = pt_desc->level;
-		pt = pt_desc->pt;
-		pt_index = meta.index[level - 1];
-
-		//L3
-		if (i == PT_LVL_MAX) {
-			if (!flags)
-				pte_flags = PTE_FLAGS_KERNEL_GENERIC;
-
-			pt[pt_index] = pt_entry(meta.offset, pte_flags);
-			break;
-		}
-
-		pte = pt[pt_index];
-		if (pte_is_empty(pte)) {
-			struct page_table_desc *new_pt_desc;
-
-			//we create next level
-			new_pt_desc = create_pt(0, level + 1);
-			pte = pt_entry((u64)new_pt_desc->pt,
-				       PTE_FLAGS_KERNEL_GENERIC);
-
-			place_pt_entry(pt_desc, pte, pt_index);
-			pt_desc = new_pt_desc;
-		} else {
-			//extract next table
-			pt_desc = (struct page_table_desc *)
-				((pte & ~(0xF0000000000FFF)) + 0x1000);
-		}
-	}
-}
-
-volatile char pt __attribute__((section(".misc"))) = 0;
+volatile char pt __section(".misc") = 0;
 static u64 set_id_translation_table(void)
 {
-	kprintf("pt: %x\n", &pt);
 	create_id_mapping(0, 0x1000 * 512, (u64)&pt, PTE_FLAGS_KERNEL_GENERIC);
 	return (u64)(global_page_tables[0]->pt);
 }
@@ -114,9 +28,31 @@ static inline void set_ttbr1_el1(u64 x)
 	asm volatile ("msr ttbr1_el1, %0"::"r"(x));
 }
 
-static inline void set_ttbr0_el1(u64 x)
+static void disable_ttbr0_el1(void)
 {
+	u64 tcr;
+
+	asm volatile ("mrs %0, tcr_el1" : "=r"(tcr));
+	tcr |= EPD_FAULT << EPD0_POS;
+	asm volatile ("msr tcr_el1, %0"::"r"(tcr));
+}
+
+static void enable_ttbr0_el1(void)
+{
+	u64 tcr;
+
+	asm volatile ("mrs %0, tcr_el1" : "=r"(tcr));
+	tcr |= EPD_WALK << EPD0_POS;
+	asm volatile ("msr tcr_el1, %0"::"r"(tcr));
+}
+
+void set_ttbr0_el1(u64 x)
+{
+	disable_ttbr0_el1();
+	mb();
 	asm volatile ("msr ttbr0_el1, %0"::"r"(x));
+	asm volatile ("dmb ish; tlbi vmalle1; isb; dmb ish;");
+	enable_ttbr0_el1();
 }
 
 void switch_vmem(void)
@@ -142,6 +78,10 @@ void switch_vmem(void)
 	asm volatile ("mov %0, lr" : "=r"(reg));
 	reg += KERNEL_START;
 	asm volatile ("mov lr, %0"::"r"(reg));
+
+	asm volatile ("mrs %0, vbar_el1" : "=r"(reg));
+	reg += KERNEL_START;
+	asm volatile ("msr vbar_el1, %0"::"r"(reg));
 
 	asm volatile ("tlbi vmalle1; dsb sy; isb");
 }
@@ -171,9 +111,7 @@ void init_mmu(void)
 	tcr |= 0b00 << SH0_POS;
 
 	asm volatile ("msr tcr_el1, %0"::"r"(tcr));
-
 	//system control SCTLR_EL1
-
 	asm volatile ("mrs %0, sctlr_el1" : "=r"(sctlr));
 
 #ifdef SCTLR_QUIRKS

pt.c
@@ -3,10 +3,12 @@
 #include <hobos/kstdio.h>
 #include <hobos/lib/pt_lib.h>
 #include <hobos/asm/barrier.h>
+#include <hobos/mmu.h>
+#include <hobos/compiler_types.h>
 
 //global list of page tables
 //TODO: make size dynamic
-struct page_table_desc *global_page_tables[10] __attribute__((section(".misc"))) ;
+__section(".misc") struct page_table_desc *global_page_tables[10];
 u8 pt_ctr;
 
 // for simplicity, we will assume that this OS only cares about
@@ -53,7 +55,7 @@ void place_pt_entry(struct page_table_desc *pt_desc, u64 pte, int index)
 //
 //returns: the first new page table entry created
 volatile u64 *create_pt_entries(struct page_table_desc *pt_desc,
-		       u64 start_paddr, u64 end_paddr,
+				u64 start_paddr, u64 end_paddr,
 		       u64 flags)
 {
 	volatile u64 *pt = pt_desc->pt;
@@ -107,6 +109,95 @@ struct page_table_desc *create_pt(u64 pt_baddr, char level)
 	global_page_tables[pt_ctr++] = pt_desc;
 
 	return pt_desc;
+}
+
+static inline void extract_va_metadata(u64 va, struct va_metadata *meta)
+{
+	u64 mask = 0xFFF;
+
+	meta->offset = va & mask;	//offset is calculated with last
+					//12 bits
+
+	mask = BITM(9);		//9 bits bitmask
+
+	meta->index[0] = (va >> 30) & mask;
+	meta->index[1] = (va >> 21) & mask;
+	meta->index[2] = (va >> 12) & mask;
+}
+
+static inline u64 pte_is_empty(u64 pte)
+{
+	return !(pte);
+}
+
+static u64 extract_addr(u64 pte)
+{
+	u64 addr = 0;
+
+	addr = pte & ~(0xF0000000000FFF);
+	if (addr < 0xFFFFFFFF)
+		return addr;
+
+	return KERNEL_START + (addr & 0xFFFFFFFF);
+}
+
+void map_pa_to_va_pg(u64 pa, u64 va, struct page_table_desc *pt_top,
+		     u64 flags)
+{
+	struct page_table_desc *pt_desc = pt_top;
+	volatile u64 *pt;
+	volatile u64 pte;
+	struct va_metadata meta;
+	u64 pte_flags = flags;
+	u16 pt_index;
+	u8 level, i;
+
+	extract_va_metadata(va, &meta);
+	meta.offset += pa;	//we want physical address at the end
+
+	if (!flags)
+		pte_flags = PTE_FLAGS_KERNEL_GENERIC;
+
+	for (i = pt_desc->level; i <= PT_LVL_MAX; i++) {
+		level = pt_desc->level;
+		pt = pt_desc->pt;
+		pt_index = meta.index[level - 1];
+
+		//L3
+		if (i == PT_LVL_MAX) {
+			pt[pt_index] = pt_entry(meta.offset, pte_flags);
+			break;
+		}
+
+		pte = pt[pt_index];
+		if (pte_is_empty(pte)) {
+			struct page_table_desc *new_pt_desc;
+
+			//we create next level
+			new_pt_desc = create_pt(0, level + 1);
+			pte = pt_entry((u64)new_pt_desc->pt, pte_flags);
+			place_pt_entry(pt_desc, pte, pt_index);
+			pt_desc = new_pt_desc;
+		} else {
+			//extract next table
+			pt_desc = (struct page_table_desc *)
+				(extract_addr(pte) + 0x1000);
+		}
+	}
+}
+
+void create_id_mapping(u64 start_paddr, u64 end_paddr,
+		       u64 pt, u64 flags)
+{
+	//for now lets assume T0/1_SZ is constant at 25, so we
+	//only care about 3 levels
+	struct page_table_desc *pt_desc;
+
+	pt_desc = create_pt(pt, 1);
+	int i;
+
+	for (i = start_paddr; i < end_paddr; i += PAGE_SIZE)
+		map_pa_to_va_pg(i, i, pt_desc, flags);
 }
 
 //traverse the page table and validate this vaddr range

commit 826fe28abd237602d57679f01b0e19a25dff582e

page allocator: Fix wrong condition for calculating page order
The page allocator is meant to allocate the next highest page order
if the current one is unavailable. The condition for validity of a
candidate order is that it should be greater than or equal to size
of the request.

Invert the current condition to fix the implemented logic.

Fixes: 60786007c8e26cde95a1e832ef1913a62b31d973 ("pt: Consolidate
page table related operations")
Signed-off-by: Utsav Agarwal <utsavagarwal2014@gmail.com>

page_alloc.c
@@ -83,7 +83,7 @@ static bool list_has_free_block(int order)
 	if (!page_block_exists(order))
 		return 0;
 
-	while ((i < list->count)) {
+	while (i < list->count) {
 		if (!pb) {
 			kprintf("WARNING: count and actual list not aligned!\n");
 			kprintf("count: [%d], elements: [%d]\n", list->count, i);
@@ -257,7 +257,6 @@ static int remove_next_free_block(int order)
 		pb = get_nth_block(list->first->order, i - 1);
 
 		if (block_is_free(pb->next)) {
-			//kprintf("removed %x\n", pb->page);
 			pb->next = 0;
 			list->count--;
 			return 0;
@@ -310,18 +309,15 @@ static int split_page_block(int order)
 	if (remove_next_free_block(curr_order))
 		return 1;
 
-	//kprintf("%x = ", pb->page);
 	// Next order buddy creation
 	page_offset = PAGE_BLOCK(order - 1) * PAGE_SIZE;
 
 	pb = new_page_block((void *)base_addr);
 	add_block_to_order(pb, order - 1);
-	//kprintf("%x + ", pb->page);
 
 	barrier();
 	pb = new_page_block((void *)get_buddy_pfn(pb));
 	add_block_to_order(pb, order - 1);
-	//kprintf("%x (%x)\n", pb->page, page_offset);
 	return 0;
 }
 
@@ -434,7 +430,7 @@ static int get_page_order(size_t nr_pages)
 	size_t val = nr_pages;
 	int i = 0;
 
-	while (val < PAGE_BLOCK(i))
+	while (val > PAGE_BLOCK(i))
 		i++;
 
 	return i;